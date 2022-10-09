a518dbfa31de5efa063e1e8ce8dc3068d4772cb7
alexsonalmeida <101877352+alexsonalmeida@users.noreply.github.com>
2022-10-09 15:45:21 -0300

    Continuacao do estudo de promises

--- javascriptAssincrono/index.js
+++ javascriptAssincrono/index.js
@@ -10,10 +10,10 @@ function esperador(msg, timer = 0) {
     return new Promise((resolve, reject) => {
         if (typeof msg !== 'string') {
             reject("Erro, tipo de dado inválido")
-            console.log("A função não morre no reject")
+            return
         }
         setTimeout(() => {
-            resolve(msg)
+            resolve('Resultado: ' + msg)
         }, timer)
     })
 }
@@ -33,6 +33,54 @@ esperador("teste inicial", criaRand())
 })
 .then(resultado => {
     console.log(resultado)
+})
+.catch(err => {
+    console.log(err)
+})
+
+//Retornando uma unica promise com Promise.all() - Se cair no catch, ele só retorna o erro
+const promises = [
+    'Teste 1',
+    esperador('Teste 2', criaRand()),
+    esperador('Teste 3', criaRand()),
+    'Teste final'
+]
+
+Promise.all(promises)
+.then(res => {
+    console.log(res)
+})
+.catch(err => {
+    console.log(err)
+})
+
+//Pegando a promise que resolver mais rápido com o Promise.race() - Só cai no catch se a primeira que for resulvida der reject
+const promises2 = [
+    esperador('Candidato 1', criaRand()),
+    esperador('Candidatp 2', criaRand()),
+    esperador('Candidato 3', criaRand()),
+    esperador('Candidato 4', criaRand())
+]
+
+Promise.race(promises2)
+.then(res => {
+    console.log(res)
+})
+.catch(err => {
+    console.log(err)
+})
+
+//Tratando de retornar uma promesa resolve ou reject diretamente como Promise.resolve() e Promise.reject()
+function baixarPagina() {
+    const emCache = true
+    
+    if (emCache)  return Promise.resolve("Página em cache") 
+    return esperador("Página baixada", criaRand())
+}
+
+baixarPagina()
+.then(msg => {
+    console.log(msg)
 })
 .catch(err => {
     console.log(err)
